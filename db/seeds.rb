e3a94c096e93b96091c6386624fbffe8e1268f77
Chris Gilbert <chrisgilbert4@icloud.com>
2020-12-04 13:47:21 +0000

    Add more db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -7,17 +9,36 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 eateries = [
-  { name: 'Pizza Express', address_id: Address.create!(building: '34 Northcote Road', city: 'London', country: 'UK').id },
-  { name: 'Honest Burgers', address_id: Address.create!(building: '534 Northcote Road', city: 'London', country: 'UK').id },
-  { name: 'Bone Daddy‘s', address_id: Address.create!(building: '12 Battersea Rise', city: 'London', country: 'UK').id },
-  { name: 'River Cafe', address_id: Address.create!(building: '314 Fulham Road', city: 'London', country: 'UK').id }
+  { 
+    name: 'Pizza Express',
+    address_id: Address.create!(building: '34 Northcote Road', city: 'London', country: 'UK').id,
+    cuisines: [Cuisine.find_or_create_by!(name: 'Pizza')]
+  },
+  { 
+    name: 'Franco Manca',
+    address_id: Address.create!(building: '112 Balham Road', city: 'London', country: 'UK').id,
+    cuisines: [Cuisine.find_or_create_by!(name: 'Pizza')]
+  },
+  { 
+    name: 'Bodeans',
+    address_id: Address.create!(building: '3234 Northcote Road', city: 'London', country: 'UK').id,
+    cuisines: [Cuisine.find_or_create_by!(name: 'Burgers')]
+  },
+  { 
+    name: 'Honest Burgers',
+    address_id: Address.create!(building: '1 Battersea Rise', city: 'London', country: 'UK').id,
+    cuisines: [Cuisine.find_or_create_by!(name: 'Burgers')]
+  },
+  { 
+    name: 'Pho',
+    address_id: Address.create!(building: '43 Battersea Rise', city: 'London', country: 'UK').id,
+    cuisines: [Cuisine.find_or_create_by!(name: 'Burgers')]
+  },
+  { 
+    name: 'Manchester Munch',
+    address_id: Address.create!(building: '1 Old Traffor', city: 'Manchester', country: 'UK').id,
+    cuisines: [Cuisine.find_or_create_by!(name: 'Burgers')]
+  },
 ]
 
-cuisines = [
-  { name: 'Italian' },
-  { name: 'French' },
-  { name: 'Burgers' }
-]
-
-eateries.each { |eatery| Restaurant.create(eatery) }
-cuisines.each { |cuisine| Cuisine.create(cuisine) }
+eateries.each { |eatery| Restaurant.create!(name: eatery[:name], address_id: eatery[:address_id]) }
