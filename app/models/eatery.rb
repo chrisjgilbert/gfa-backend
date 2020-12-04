31f7d329b9b53887798df49369fbcf32ceedc31a
Chris Gilbert <chrisgilbert4@icloud.com>
2020-12-04 13:25:12 +0000

    Remove accepts nested attributes for

--- app/models/eatery.rb
+++ app/models/eatery.rb
@@ -2,5 +2,4 @@ class Eatery < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   has_and_belongs_to_many :cuisines
   has_one :address
-  accepts_nested_attributes_for :address
 end
